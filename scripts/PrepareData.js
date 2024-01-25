65cb62498bcfddb5ef2776dde4d046217dfe2b1a
lexuanbinh <lb@silverliningresearch.com>
2024-01-25 14:19:57 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -175,17 +175,21 @@ function prepareInterviewData() {
     var quota_month =  quota_data_temp[i].Month + "-"  + quota_data_temp[i].Year; 
     if (quota_month== currentMonth)
     {
+      if (quota_data_temp[i].Airport_Airline =="DOH-QR") {
+        quota_data_temp[i].Quota = quota_data_temp[i].Quota - 50;
+      }
+      if (quota_data_temp[i].Airport_Airline =="DOH-AY") {
+        quota_data_temp[i].Quota = quota_data_temp[i].Quota + 50;
+      }
+
       if (currentMonth=="11-2023") {//special for 08-2023 as there is still capacity.
         quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota*1.25);
       }
       if (currentMonth=="12-2023") {//special for 08-2023 as there is still capacity.
         quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota*1.15);
       }
-      if (quota_data_temp[i].Airport_Airline =="DOH-QR") {
-        quota_data_temp[i].Quota = quota_data_temp[i].Quota - 50;
-      }
-      if (quota_data_temp[i].Airport_Airline =="DOH-AY") {
-        quota_data_temp[i].Quota = quota_data_temp[i].Quota + 50;
+      if (currentMonth=="01-2024") {//special for 08-2023 as there is still capacity.
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota*1.15);
       }
 
       quota_data.push(quota_data_temp[i]);
