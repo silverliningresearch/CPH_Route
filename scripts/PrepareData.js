73b402fc7ae56c7b3b74d246f1eab3ed03832f09
lexuanbinh <lb@silverliningresearch.com>
2024-03-25 17:59:05 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -213,6 +213,8 @@ function prepareInterviewData() {
 
       if (currentMonth=="03-2024") 
       {
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota*1.17);
+
         if (quota_data_temp[i].Dest == "DEL") {
           quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota*3); //57==> 157
         }
