eca64420b5489295649a577781ea0c11e981e0f2
lexuanbinh <lb@silverliningresearch.com>
2025-07-07 16:50:59 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -596,6 +596,18 @@ function prepareInterviewData() {
       }
     }
           
+    if (currentMonth == "07-2025") 
+    {
+
+      if ((quota_data_temp[i].Dest == "DOH")) {
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota) + 50;
+      }
+
+      if ((quota_data_temp[i].Dest == "DXB")) {
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota) + 50;
+      }      
+
+    }    
       quota_data.push(quota_data_temp[i]);
     }
   }
