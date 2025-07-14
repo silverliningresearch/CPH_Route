eeb5cbc5ca0fc8ded62dbd15bc4995ed452d8e9b
lexuanbinh <lb@silverliningresearch.com>
2025-07-14 19:18:12 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -607,6 +607,10 @@ function prepareInterviewData() {
         quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota) + 50;
       }      
 
+      if ((quota_data_temp[i].Dest == "PVG")) {
+        quota_data_temp[i].Quota =  50;
+      }      
+
     }    
       quota_data.push(quota_data_temp[i]);
     }
