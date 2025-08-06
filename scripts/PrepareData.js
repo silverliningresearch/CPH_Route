7bbb424a3d319c1aa23b6f6fb9c823dc4e6798ec
lexuanbinh <lb@silverliningresearch.com>
2025-08-06 12:16:52 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -613,7 +613,24 @@ function prepareInterviewData() {
       
       quota_data_temp[i].Quota =  Math.round(quota_data_temp[i].Quota*1.15);  
          
-    }    
+    }   
+    
+    if (currentMonth == "08-2025") 
+    {
+      if ((quota_data_temp[i].Dest == "DOH")) {
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota) + 50;
+      }
+
+      if ((quota_data_temp[i].Dest == "DXB")) {
+        quota_data_temp[i].Quota = Math.round(quota_data_temp[i].Quota) + 50;
+      }      
+
+      if ((quota_data_temp[i].Airport_Airline == "JFK-SK")) {
+        quota_data_temp[i].Quota = quota_data_temp[i].Quota + 50;
+      }  
+         
+    }   
+    
       quota_data.push(quota_data_temp[i]);
     }
   }
